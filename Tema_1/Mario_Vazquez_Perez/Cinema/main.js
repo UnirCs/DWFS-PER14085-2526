26e99fadacee9ada8fa1e528d98496afdd976f47
MavapeGZ <mavape4403@gmail.com>
2025-11-05 16:50:02 +0100

    fix: Minor changes to avoid SonarQube warnings

--- Tema_1/Mario_Vazquez_Perez/Cinema/main.js
+++ Tema_1/Mario_Vazquez_Perez/Cinema/main.js
@@ -1,18 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const seats = document.querySelectorAll("button.seat");
 
-    seats.forEach(seat => {
+    for (const seat of seats) {
         seat.addEventListener("click", () => {
             if (seat.classList.contains("seat--occupied")) {
                 return;
             }
             if (seat.classList.contains("seat--wheelchair")) {
-                if (seat.classList.contains("seat--wheelchair")) {
-                    seat.classList.toggle("seat--wheelchair-active");
-                    return;
-                }
+                seat.classList.toggle("seat--wheelchair-active");
+                return;
             }
             seat.classList.toggle("seat--selected");
         });
-    });
+    }
 });
