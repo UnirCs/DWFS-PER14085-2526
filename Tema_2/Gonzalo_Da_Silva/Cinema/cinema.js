6d89e6d7600d20d839afcec1fe9f7aecd3e2c943
Gonzalo Da Silva <gonzalomauriciodt@gmail.com>
2025-11-24 23:49:30 -0500

    feat: task 2

---
+++ Tema_2/Gonzalo_Da_Silva/Cinema/cinema.js
@@ -0,0 +1,52 @@
+// Definir el tamaño de la matriz de butacas
+const N = 10; // Número de filas y columnas
+
+// Función para inicializar la matriz de butacas
+function setup() {
+    let idContador = 1; // Iniciar el contador de IDs en 1 (los humanos no empezamos a contar desde 0)
+    let butacas = [];
+
+    for (let i = 0; i < N; i++) {
+        // Nueva fila
+        let fila = [];
+        for (let j = 0; j < N; j++) {
+            // Nuevo asiento
+            fila.push({
+                id: idContador++,
+                estado: false // Estado inicial libre
+            });
+        }
+        butacas.push(fila);
+    }
+
+    butacas[0][3].estado = true;
+    butacas[0][4].estado = true;
+    butacas[9][5].estado = true;
+    butacas[2][6].estado = true;
+    butacas[2][7].estado = true;
+    butacas[5][0].estado = true;
+    butacas[5][9].estado = true;
+    butacas[8][4].estado = true;
+    butacas[8][5].estado = true;
+    butacas[9][9].estado = true;
+
+    return butacas;
+}
+
+const suggest = (butacas, reservations) => {
+    if (reservations > N) return new Set();
+
+    for (let i = butacas.length - 1; i >= 0; i--) {
+        for (let j = 0; j <= N - reservations; j++) {
+            const asientos = butacas[i].slice(j, j + reservations);
+            if (asientos.every(a => !a.estado)) {
+                return new Set(asientos.map(a => a.id));
+            }
+        }
+    }
+
+    return new Set();
+}
+
+
+console.log(suggest(setup(), 10));
