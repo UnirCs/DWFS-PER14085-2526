5f1f63e09058ccf5a558d4312520893d1d53e286
SouljahBlush <rairendon@hotmail.com>
2025-12-09 14:14:57 -0500

    Tema 5

--- Tema_5/Rainiero Rendon Lujano/src/main.jsx
+++ Tema_5/Rainiero Rendon Lujano/src/main.jsx
@@ -4,7 +4,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Footer } from './components/Footer.jsx'
-import { Header } from './components/header.jsx'
+import { Header } from './components/Header.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
